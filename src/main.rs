fe42cd15819307afec17de72d3c14644c87d792c
hexaredecimal <mfanakagama@gmail.com>
2024-08-28 01:22:30 +0200

    hexarewm: Code refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,15 @@
+use hexawm::{
+    colordefs::WmColors, keybinds::raw_key_bindings, layouts::WmLayout, statusbar::WmStatusBar,
+    wmconfig::WmConfig,
+};
 use penrose::{
     core::{bindings::parse_keybindings_with_xmodmap, Config, WindowManager},
     extensions::hooks::add_ewmh_hooks,
     x11rb::RustConn,
     Result,
 };
-
 use penrose_ui::bar::Position;
 use std::collections::HashMap;
-
-use hexawm::{
-    keybinds::raw_key_bindings, layouts::WmLayout, statusbar::WmStatusBar, wmconfig::WmConfig,
-};
-
-const BLACK: u32 = 0x282828ff;
-const GREY: u32 = 0x3c3836ff;
 
 fn main() -> Result<()> {
     let config = add_ewmh_hooks(Config {
@@ -23,15 +19,18 @@ fn main() -> Result<()> {
 
     let conn = RustConn::new()?;
     let key_bindings = parse_keybindings_with_xmodmap(raw_key_bindings())?;
-    let wm_config = WmConfig::default();
 
-    let bar = WmStatusBar::status_bar(&wm_config, BLACK, GREY, Position::Top);
+    let bar = WmStatusBar::status_bar(
+        &mut WmConfig::default(),
+        WmColors::black(),
+        WmColors::grey(),
+        Position::Top,
+    );
     let wm = bar.add_to(WindowManager::new(
         config,
         key_bindings,
         HashMap::new(),
         conn,
     )?);
-
     wm.run()
 }
